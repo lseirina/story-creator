c07e42f7aae5f488dde289da60bd8eaee16bab29
root <lseirina87@gmail.com>
2024-08-23 17:21:22 +0300

    Added register story in admin

--- app/stories/models.py
+++ app/stories/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class StoryModel(models.Model):
+class Story(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    text = models.TextField()
-    created_at = models.DataTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- app/stories/tests.py
+++ app/stories/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from stories import models
 
+class ModelTests(TestCase):
+    """ Tests for models."""
 
+    def create_story(self):
+        story = models.Story.objects.create(
+            title='Unicorn',
+            content='Blah, blah, blah'
+        )
+
+        self.assertEqual(str(story, story.title))
