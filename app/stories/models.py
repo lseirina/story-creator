7a2cc49fa9674a5614943eeaaf89c061eb4c79e7
root <lseirina87@gmail.com>
2024-08-25 21:41:31 +0300

    Added VoiceRecording models

--- app/stories/models.py
+++ app/stories/models.py
@@ -11,9 +11,15 @@ class Story(models.Model):
 
 
 class VoiceRecording(models.Model):
+    """related_name задает имя обратной связи, через которое обращаются
+    к записям (VoiceRecording),
+    связанным с определенной историей (Story).
+    Без использования related_name доступ к связанным объектам происходит
+    через автоматическое имя <model_name>_set,
+    в вашем случае это было бы voicerecording_set."""
     story = models.ForeignKey(Story, related_name='recordings',
                               on_delete=models.CASCADE)
-    file = models.FileField(upload_to='recordings/')
+    file = models.FileField(upload_to='recordings/') # Параметр upload_to определяет путь, куда будут загружаться файлы на сервере.
     transcription = models.TextField(blank=True, null=True)
 
     def __str__(self):
