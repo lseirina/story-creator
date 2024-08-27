a24f12a2c148f386d9959c4a2f5c1da5944e11fa
root <lseirina87@gmail.com>
2024-08-27 17:01:18 +0300

    Added test recording form valid

--- app/stories/tests/test_forms.py
+++ app/stories/tests/test_forms.py
@@ -2,7 +2,10 @@
 Tests forms.
 """
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from stories.forms import StoryForm, VoiceRecordingForm
+from stories.models import Story
+import tempfile
 
 
 class FormTest(TestCase):
@@ -22,3 +25,20 @@ class FormTest(TestCase):
         form = VoiceRecordingForm(data=form_data)
 
         self.assertFalse(form.is_valid())
+
+    def test_recording_form_valid(self):
+        """Test record audio file if form is valid."""
+        audio_file = SimpleUploadedFile('sample.mp3',
+                                        b'file_content',
+                                        content_type='audio/mpeg')
+        form = VoiceRecordingForm(files={'file': audio_file})
+        story = Story.objects.create(title='Sample title',
+                                     content='it is content')
+
+        self.assertTrue(form.is_valid())
+
+        recording = form.save(commit=False) # останавливает автоматическое сохранение формы в базу данных,
+        # чтобы сделать дополнительные изменения перед сохранением
+        recording.story = story
+        recording.save()
+        self.assertEqual(recording.file.size, len(b'file_content'))
