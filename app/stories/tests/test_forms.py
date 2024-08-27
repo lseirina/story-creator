089706d3bd48a4c5d7f2591ba988d20d9c4a3dbc
root <lseirina87@gmail.com>
2024-08-27 15:48:29 +0300

    Added test create story if form valid

---
+++ app/stories/tests/test_forms.py
@@ -0,0 +1,17 @@
+"""
+Tests forms.
+"""
+from django.test import TestCase
+from stories.forms import StoryForm, VoiceRecordingForm
+
+
+class FormTest(TestCase):
+    def test_story_form_valid(self):
+        """Test create story if form is valid."""
+        form_data = {'title': 'Sample title', 'content': 'It is content'}
+        form = StoryForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+
+        story = form.save()
+        self.assertEqual(story.title, form_data['title'])
