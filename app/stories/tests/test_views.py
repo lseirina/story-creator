7817c7a1bc4a045516b3d0e529b621c56e1a3606
root <lseirina87@gmail.com>
2024-08-28 16:23:51 +0300

    Added test_context_data

--- app/stories/tests/test_views.py
+++ app/stories/tests/test_views.py
@@ -6,6 +6,7 @@ from stories.views import story_list
 
 STORIES_URL = reverse('story_list')
 
+
 class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
@@ -16,3 +17,15 @@ def test_view_correct_templates(self):
     res = self.client.get(STORIES_URL)
     self.assertEqual(res.status_code, 200)
     self.assertTemplateUsed(res, 'stories/story_list.html')
+
+
+def test_context_data(self):
+    """Test the view passes correct context data."""
+    Story.objects.create(title='Story 1', content='It as a content')
+    Story.objects.create(title='Story 2', content='It is a content')
+
+    res = self.client.get(STORIES_URL)
+
+    self.assertequal(res.status_code, 200)
+    self.assertIn('stories', res.context )
+    self.assertEqual(len(res.context['syories'], 2))
