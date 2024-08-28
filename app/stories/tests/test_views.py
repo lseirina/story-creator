d6ba7745b6b45f785c8a2e9667022b2956c8e710
root <lseirina87@gmail.com>
2024-08-28 20:38:19 +0300

    Added add_recording view

--- app/stories/tests/test_views.py
+++ app/stories/tests/test_views.py
@@ -67,6 +67,7 @@ def test_view_coorect_templates(self):
     self.assertEqual(res.status_code, 200)
     self.assertTemplateUsed(res, 'stories/create_story.html')
 
+
 def test_story_detail_correct_content(self):
     """Test story_detail view contains correct cintent."""
     story = create_story()
--- app/stories/views.py
+++ app/stories/views.py
@@ -4,16 +4,19 @@ from stories.forms import StoryForm, VoiceRecordingForm
 
 
 def story_list(request):
+    """View for listings stories."""
     stories = Story.objects.all()
     return render(request, 'stories/story_list.html', {'stories': stories})
 
 
 def story_detail(request, story_id):
+    """View for detail story."""
     story = Story.objects.get(id=story_id)
     return render(request, 'stories/story_detail.html', {'story': story})
 
 
 def create_story(request):
+    """View to create a story."""
     if request.method == 'POST':
         form = StoryForm(request.POST)
         if form.is_valid:
@@ -22,6 +25,21 @@ def create_story(request):
         else:
             form = StoryForm()
     return render(request, 'stories/create_story.html', {'form': form})
+
+
+def add_recording(request, story_id):
+    """View to add story recording."""
+    if request.method == 'POST':
+        form = VoiceRecordingForm(request.POST, request.FILES)
+        if form.is_valid():
+            recording = form.save(commit=False)
+            recording.story_id = story_id
+            recording.save()
+            return redirect('story_detail', story_id=story_id)
+        else:
+            recording = VoiceRecordingForm()
+    return render(request, 'stories/add_recording.html', {'form': form})
+
 
 # добавить предсавление для put patch и story_detail помимо остальных
 
