1b1128e1af481693c8836be59481ccfa3e302404
root <lseirina87@gmail.com>
2024-09-02 20:23:30 +0300

    Added urls.py

--- app/story_creator/urls.py
+++ app/story_creator/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/', views.create_story, name='create_story'),
     path('<int:story_id>/add_recording/', views.add_recording, name='add_recording'),
-    path('recording/<int:recording_id>/edit/',
+    path('<int:recording_id>/edit/',
          views.edit_transcription, name='edit_transcription')
 ]
